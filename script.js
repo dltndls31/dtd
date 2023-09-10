07a004bbb08461f7911cb96724671f2257383dd3
dltndls31 <dtd131@naver.com>
2023-09-10 20:10:07 +0900

    메시지

---
+++ script.js
@@ -0,0 +1,74 @@
+const postForm = document.getElementById('post-form')
+const postTitleInput = document.getElementById('post-title')
+const postContentInput = document.getElementById('post-content')
+const postList = document.getElementById('post-list')
+
+postForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const title = postTitleInput.value
+    const content = postContentInput.value
+
+    if (title.trim() === '' || content.trim() === '') {
+        alert('제목과 내용을 모두 입력하세요.')
+        return
+    }
+
+    const postDiv = document.createElement('div')
+    postDiv.className = 'post'
+    postDiv.innerHTML = `<h2>${title}</h2><p>${content}</p>`
+
+    const editButton = document.createElement('button')
+    editButton.className = 'edit'
+    editButton.textContent = '수정'
+
+    editButton.addEventListener('click', (e) => {
+        const currentTitle = postDiv.querySelector('h2')
+        const currentContent = postDiv.querySelector('p')
+
+        const titleInput = document.createElement('input')
+        titleInput.value = currentTitle.textContent
+        const contentTextarea = document.createElement('textarea')
+        contentTextarea.value = currentContent.textContent
+
+        const saveButton = document.createElement('button')
+        saveButton.textContent = '수정한 내용 저장'
+
+        saveButton.addEventListener('click', () => {
+            currentTitle.textContent = titleInput.value
+            currentContent.textContent = contentTextarea.value
+            postDiv.replaceChild(currentTitle, titleInput)
+            postDiv.replaceChild(currentContent, contentTextarea)
+            postDiv.removeChild(saveButton)
+            postDiv.appendChild(editButton)
+        })
+
+        postDiv.replaceChild(titleInput, currentTitle)
+        postDiv.replaceChild(contentTextarea, currentContent)
+        postDiv.appendChild(saveButton)
+        postDiv.removeChild(editButton)
+    })
+
+    const deleteButton = document.createElement('button')
+    deleteButton.className = 'delete'
+    deleteButton.textContent = '삭제'
+    deleteButton.addEventListener('click', (e) => {
+        postList.removeChild(postDiv)
+    })
+
+    // 심화 과제: 즐겨찾기 버튼
+    const favoriteButton = document.createElement('button')
+    favoriteButton.className = 'favorite'
+    favoriteButton.textContent = '즐겨찾기'
+    favoriteButton.addEventListener('click', () => {
+        postList.insertBefore(postDiv, postList.firstChild)
+    })
+
+    postDiv.appendChild(editButton)
+    postDiv.appendChild(deleteButton)
+    postDiv.appendChild(favoriteButton)
+    postList.insertBefore(postDiv, postList.firstChild)
+
+    postTitleInput.value = ''
+    postContentInput.value = ''
+})
